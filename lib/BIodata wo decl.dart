7503ec748c437f331173824d17305b84b739e7af
DiyaShajith <diyashajith.2231@gmail.com>
2024-04-18 12:55:37 +0530

    18-04-2024

---
+++ lib/BIodata wo decl.dart
@@ -0,0 +1,60 @@
+import 'dart:io';
+
+void main()
+{
+  print("Enter Your Name:");
+  var name=stdin.readLineSync();
+  print("Name :$name");
+  print("Enter Father Name:");
+  var father=stdin.readLineSync();
+  print("Father Name :$father");
+  print("Enter Father's occupation :");
+  var occ=stdin.readLineSync();
+  print("Father Occupation :$occ");
+  print("Enter Date of Birth :");
+  var dt=stdin.readLineSync();
+  int date=int.parse(dt!);
+  print("Date-of-Birth :$date");
+  print("Enter your Address :");
+  var add=stdin.readLineSync();
+  print("Address :$add");
+  print("Enter Contact Number :");
+  var cn=stdin.readLineSync();
+  int contact=int.parse(cn!);
+  print("Contact Number :$contact");
+  print("Enter you sex :");
+  var sx=stdin.readLineSync();
+  print("Sex :$sx");
+  print("Enter your Marital status :");
+  var mr=stdin.readLineSync();
+  print("Marital Status :$mr");
+  print("Enter your Religion:");
+  var r=stdin.readLineSync();
+  print("Religion :$r");
+  print("Enter your Community:");
+  var cm=stdin.readLineSync();
+  print("Community :$cm");
+  print("Enter your Nationality :");
+  var n=stdin.readLineSync();
+  print("Nationality :$n");
+  print("Enter your Educational Qualification :");
+  var eq=stdin.readLineSync();
+  print("Esucational Qualification :$eq");
+  print("Enter Language Known :");
+  var lk=stdin.readLineSync();
+  print("Language Known :$lk");
+  print("Enter your Experience :");
+  var ex=stdin.readLineSync();
+  print("Experience :$ex");
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ lib/largest 3 numbers.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+void main()
+{
+  print("Enter 3 numbers : ");
+  var a=stdin.readLineSync();
+  double num1=double.parse(a!);
+  var b=stdin.readLineSync();
+  double num2=double.parse(b!);
+  var c=stdin.readLineSync();
+  double num3=double.parse(c!);
+  if(num1>num2) {
+    if (num1 > num3) {
+      print("A is greatest :$num1\n");
+    }
+    else {
+      print("C is greatest :$num3\n");
+    }
+  }
+      else
+        {
+          if(num2>num3)
+            {
+              print("B is greatest : $num2\n");
+            }
+          else
+            {
+              print("C is greatest : $num3\n");
+            }
+    }
