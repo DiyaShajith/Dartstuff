4f5e45eef6792c567daae98c9a59f7556395fd79
DiyaShajith <diyashajith.2231@gmail.com>
2024-04-19 07:19:52 +0530

    19-04-2024

---
+++ lib/do while.dart
@@ -0,0 +1,9 @@
+void main()
+{
+  int i=0;
+  do
+  {
+    print("Numbers are :$i");
+    i++;
+  }while(i<=10);
+}
---
+++ lib/while.dart
@@ -0,0 +1,9 @@
+void main()
+{
+  int i=0;
+  while(i<=10)
+    {
+      print("Numbers are :$i");
+      i++;
+    }
+}
