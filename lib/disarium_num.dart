38c929933d922898612843e7723d9c8ce13e8a4d
DiyaShajith <diyashajith.2231@gmail.com>
2024-08-13 11:22:28 +0530

    disarium_num

--- lib/disarium_num.dart
+++ lib/disarium_num.dart
@@ -3,24 +3,22 @@ import 'dart:math';
 bool isDisarium(int number) {
   int sum = 0;
   int length = number.toString().length;
-  
-  // Create a temporary variable to iterate through the digits
   int temp = number;
-  
   for (int i = length; i > 0; i--) {
     int digit = temp % 10;
-    sum += pow(digit, i).toInt();
+    int b = pow(digit, i).toInt();
+    sum = sum + b;
+
     temp ~/= 10;
   }
-  
   return sum == number;
 }
 
 void main() {
-  int number = 135;
-  if (isDisarium(number)) {
-    print('$number is a Disarium number.');
+  int a = 135;
+  if (isDisarium(a)) {
+    print('$a is a Disarium number.');
   } else {
-    print('$number is not a Disarium number.');
+    print('$a is not a Disarium number.');
   }
 }
