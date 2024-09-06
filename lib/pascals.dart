b7d40f00fe966d93f1cd8cfb413a278c0b261325
Diya Shajith K <diyashajith.2231@gmail.com>
2024-09-06 10:52:54 +0530

    singlenumber

--- lib/pascals.dart
+++ lib/pascals.dart
@@ -1,21 +1,31 @@
-import 'dart:io';
+// import 'dart:io';
 
-void main()
-{
-  print("Enter the row");
-  int n=int.parse(stdin.readLineSync()!);
-  for(int i=0;i<n;i++)
+// void main()
+// {
+//   print("Enter the row");
+//   int n=int.parse(stdin.readLineSync()!);
+//   for(int i=0;i<n;i++)
 
-    {int number=1;
-      for(int s=0; s<n-i; s++)
-        {
-          stdout.write(" ");
-        }
-      for(int j=0;j<=i;j++)
-        {
-          stdout.write("$number ");
-          number=number*(i-j)~/(j+1);
-        }
-      stdout.write("\n");
-    }
+//     {int number=1;
+//       for(int s=0; s<n-i; s++)
+//         {
+//           stdout.write(" ");
+//         }
+//       for(int j=0;j<=i;j++)
+//         {
+//           stdout.write("$number ");
+//           number=number*(i-j)~/(j+1);
+//         }
+//       stdout.write("\n");
+//     }
+// }
+void main() {
+  var numbers = [122, 12, 33, 14, 5];
+  numbers.sort();
+
+  if (numbers.first == numbers.last) {
+    print("No single number found. Returning null.");
+  } else {
+    print("Largest single number: ${numbers.last}");
+  }
 }
