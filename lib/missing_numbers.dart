8d63f696581e08f23f18c7f2ade9155c26c2a5c8
Diya Shajith K <diyashajith.2231@gmail.com>
2024-10-04 10:51:24 +0530

    missing number completed

--- lib/missing_numbers.dart
+++ lib/missing_numbers.dart
@@ -1,14 +1,29 @@
+import 'dart:io';
+
 void main() {
-  List<int> nums = [2,5];
+  List<int> nums = [];
+
+  print('Enter the number of elements in the list:');
+  int n = int.parse(stdin.readLineSync()!);
+
+  print('Enter the $n elements:');
+  for (var i = 0; i < n; i++) {
+    int element = int.parse(stdin.readLineSync()!);
+    nums.add(element);
+  }
+
   List<int> newList = [];
   List<int> missing = [];
+
   for (var i = 0; i < nums.last; i++) {
     newList.add(i + 1);
   }
+
   for (var i = 0; i < newList.length; i++) {
     if (!nums.contains(newList[i])) {
       missing.add(newList[i]);
     }
   }
-print(missing);
+
+  print('Missing numbers: $missing');
 }
