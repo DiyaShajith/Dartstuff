99fc2aa89866c6089b198de7cede44cc54516e25
Diya Shajith K <diyashajith.2231@gmail.com>
2024-10-04 10:55:39 +0530

    first unique character completed

--- lib/firstunique_character.dart
+++ lib/firstunique_character.dart
@@ -1,3 +1,31 @@
 void main(){
-    
+    int firstUniqChar(String s) {
+  Map<String, int> charCount = {};
+
+
+  for (int i = 0; i < s.length; i++) {
+    charCount[s[i]] = (charCount[s[i]] ?? 0) + 1;
+  }
+
+
+  for (int i = 0; i < s.length; i++) {
+    if (charCount[s[i]] == 1) {
+      return i;
+    }
+  }
+
+  return -1;
+}
+
+void main() {
+  String s = "leetcode";
+  print(firstUniqChar(s)); 
+
+  String s2 = "loveleetcode";
+  print(firstUniqChar(s2)); 
+
+  String s3 = "aabb";
+  print(firstUniqChar(s3));
+}
+
 }
